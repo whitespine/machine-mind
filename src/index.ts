37905c859891ed43d6f4373ea8e6047701b5ab7e
whitespine <jakehenry.tn@gmail.com>
2020-11-16 23:10:45 -0500

    Made the export interface less annoying, lol

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-export * as types from "@src/interface";
-export * as classes from "@src/class";
+export * from "@src/interface";
+export * from "@src/class";
+
+// Group these for clarity
 export * as enums from "@src/enums";
 export * as funcs from "@src/funcs";
 export * as typed_lancer_data from "@src/classes/utility/typed_lancerdata";
 
-export * as registry from "@src/registry";
-export * as util_registry from "@src/classes/regstack";
-export * as static_registry from "@src/static_registry";
 
-// export * as typed_lancer_data from "@src/typed_lancerdata";
-
-// import * as hooks from "@src/hooks";
-// export { hooks };
+// Bundle these for clarity
+import * as r1 from "@src/registry";
+import * as r2 from "@src/classes/regstack";
+import * as r3 from "@src/static_registry";
+export const registry = {...r1, ...r2, ...r3};
