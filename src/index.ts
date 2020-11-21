3113db7a71af14d75bcdb666279581fb5e3f4936
whitespine <jakehenry.tn@gmail.com>
2020-11-21 16:18:00 -0500

    Further relaxed exports because I was being dumb

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,12 @@ export * from "@src/interface";
 export * from "@src/class";
 
 // Group these for clarity
-export * as enums from "@src/enums";
+export * from "@src/enums";
 export * as funcs from "@src/funcs";
 export * as typed_lancer_data from "@src/classes/utility/typed_lancerdata";
 
 
 // Bundle these for clarity
-import * as r1 from "@src/registry";
-import * as r2 from "@src/classes/regstack";
-import * as r3 from "@src/static_registry";
-export const registry = {...r1, ...r2, ...r3};
+export * from "@src/registry";
+export * from "@src/classes/regstack";
+export * from "@src/static_registry";
